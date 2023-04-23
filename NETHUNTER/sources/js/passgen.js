547f9d9f41d5f2957ba51d50221ffd8d3bc779b0
IceHeart01 <131564122+IceHeart01@users.noreply.github.com>
2023-04-23 03:54:25 +0300

    Delete NETHUNTER directory

--- NETHUNTER/sources/js/passgen.js
+++
@@ -1,9 +0,0 @@
-var site, salt;
-var text, password;
-function generate() {
-  site = document.getElementById("site_url").value;
-  salt = document.getElementById("keyword").value;
-  text = site + salt + "j93J$iu3ku#*fKJWUG83KWUEG8u*# Ku$#jG84KJ";
-  password = md5(text);
-  document.getElementById("pass_text").innerHTML = password;
-}
